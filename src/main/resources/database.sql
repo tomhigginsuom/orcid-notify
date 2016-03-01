2259b7a7f4e7c8acbd95153b6f4b0f54ebfa7298
Tom Higgins <thomas.higgins@manchester.ac.uk>
2016-03-01 18:06:17 +0000

    Updated test db schema

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,10 +1,21 @@
 CREATE TABLE orcid (
-  orcid_id varchar(19) NOT NULL,
+  orcid_id VARCHAR(19) NOT NULL,
+  family_name VARCHAR(255),
+  given_names VARCHAR(255),
   updated TIMESTAMP NULL,
+  needs_update BOOLEAN DEFAULT NULL,
   PRIMARY KEY (orcid_id)
 );
 
--- CREATE USER 'orcid'@'localhost' IDENTIFIED BY 'orcid';
--- CREATE USER 'orcid'@'%' IDENTIFIED BY 'orcid';
--- GRANT ALL PRIVILEGES ON *.* TO 'orcid'@'localhost';
--- GRANT ALL PRIVILEGES ON *.* TO 'orcid'@'%';
+CREATE TABLE works (
+  orcid_id VARCHAR(19) NOT NULL,
+  put_code INT NOT NULL,
+  identifier_type VARCHAR(50) NOT NULL,
+  identifier VARCHAR(255) NOT NULL,
+  title VARCHAR(1000),
+  created TIMESTAMP,
+  publication_day SMALLINT,
+  publication_month SMALLINT,
+  publication_year SMALLINT,
+  PRIMARY KEY (orcid_id, put_code, identifier_type, identifier)
+);
